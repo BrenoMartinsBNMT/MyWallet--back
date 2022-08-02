3abf6d9a7b7f3299492bc66bc64147db8acedbf8
breno <brenoamrtinscunha@gmail.com>
2022-08-02 19:18:07 -0300

    make it better

--- dist/src/errors/errosTransactions.js
+++
@@ -1,5 +0,0 @@
-export function isTokenValid(token) {
-    if (!token) {
-        throw { error: 401, message: "usuário não autorizado!" };
-    }
-}
--- dist/src/schemas/schemaTransactions.js
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-export var schemaGetTransactions = Joi.object({
-    token: Joi.string().required()
-});
-export var schemaBalance = Joi.object({
-    balance: Joi.number().greater(0).required(),
-    token: Joi.string().required(),
-    description: Joi.string().required()
-});

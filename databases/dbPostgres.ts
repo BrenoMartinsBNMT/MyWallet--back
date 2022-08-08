60ab4b2e0614a11dd74f568ac0e19fdaf62b2f91
Breno Martins <brenomartinscunha@gmail.com>
2022-08-08 00:30:17 -0300

    deploy

--- databases/dbPostgres.ts
+++ databases/dbPostgres.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 dotenv.config();
 
 const { Pool } = pg;
-
+/* 
 const user = process.env.USER_DB;
 const password = process.env.PASSWORD;
 const host = process.env.HOST;
@@ -17,5 +17,13 @@ const db = new Pool({
   port,
   database,
 });
+ */ const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+const db = new Pool(databaseConfig);
 
 export default db;
